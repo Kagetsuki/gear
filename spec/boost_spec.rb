961e56b5a53730047e6d2349c2c8459508d8d909
影月 零 <zero@genshin.org>
2015-11-20 04:18:50 +0900

    fix boost spec for systems that have boost installed

--- spec/boost_spec.rb
+++ spec/boost_spec.rb
@@ -5,10 +5,14 @@ describe Gears::Boost do
   boost = Gears::Boost.new
 
   it 'builds/installs' do
-    expect(boost.engage).to eq(true)
+    #expect(boost.engage).to eq(true)
+    boost.obtain
     expect(boost.obtained).to eq(true)
+    boost.build
     expect(boost.built).to eq(true)
+    boost.install
     expect(boost.installed).to eq(true)
+    boost.check
     expect(boost.checked).to eq(true)
   end
 end
