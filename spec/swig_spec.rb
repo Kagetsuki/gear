dd159d997a1a103e3cd4e9112caecca20d22fdc0
影月 零 <zero@genshin.org>
2015-11-20 04:13:49 +0900

    refactor swig spec to take into account linearity of tests

--- spec/swig_spec.rb
+++ spec/swig_spec.rb
@@ -9,32 +9,23 @@ describe Gears::SWIG do
     expect(swig.name).to eq('SWIG')
   end
 
-  it 'obtains SWIG sources from github' do
+  it 'obtains, builds, installs, checks, uninstalls and removes SWIG' do
     expect(swig.obtain).to eq(true)
     expect(swig.obtained).to eq(true)
-  end
 
-  it 'builds SWIG' do
     expect(swig.build).to eq(true)
     expect(swig.built).to eq(true)
-  end
 
-  it 'installs (vendorizes) SWIG' do
     expect(swig.install).to eq(true)
     expect(swig.installed).to eq(true)
-  end
 
-  it 'checks SWIG was installed and is usable' do
     expect(swig.check).to eq(true)
     expect(swig.checked).to eq(true)
-  end
 
-  it 'uninstalls SWIG' do
+
     expect(swig.uninstall).to eq(true)
     expect(swig.installed).to eq(false)
-  end
 
-  it 'removes the SWIG repository from the build directory' do
     expect(swig.remove).to eq(true)
     expect(swig.obtained).to eq(false)
   end
